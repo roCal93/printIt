20ceb751f3ff4f387d9940b342fc12a0e9402a9d
Romain Calmelet <romaincalmelet@gmail.com>
2024-09-13 07:47:39 +0200

    More_comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,14 @@
-// Display img & txt function 
-function displayImgTxt(actualPosition, list) {
+// Function that recovers the position of the image and text in the html and replaces the data with the one in the array.
+function displayImgTxt(currentPosition, list) {
   // Img
   const img = document.querySelector(".banner-img")
-  img.src = `./assets/images/slideshow/${list[actualPosition].image}`
+  img.src = `./assets/images/slideshow/${list[currentPosition].image}`
   // Txt
   const txt = document.querySelector("#banner p")
-  let txtP = `<p>${list[actualPosition].tagLine}<p>`
-  txt.innerHTML = txtP
+  txt.innerHTML = `<p>${list[currentPosition].tagLine}<p>`
 }
 
-// Dots integration
+// Function that creates an empty dot for each object in the array and fills the first dot.
 function dotsIntegration() {
   const dots = document.querySelector(".dots")
   for (let i = 0; i < slides.length; i++) {
@@ -22,12 +21,12 @@ function dotsIntegration() {
   }
 }
 
-// Function dot selected 
-function dotSelected(actualPosition) {
+// Function that fills the dot if it matches the current position and removes it if it does not.
+function dotSelected(currentPosition) {
   const listDots = document.querySelectorAll(".dot")
   for (let i = 0; i < listDots.length; i++) {
-    dot = listDots[i]
-    if (i === actualPosition) {
+    let dot = listDots[i]
+    if (i === currentPosition) {
       dot.classList.add("dot_selected")
     }
     else {
@@ -36,16 +35,18 @@ function dotSelected(actualPosition) {
   }
 }
 
-// Change banner after each clic on arrows
+// Main function who changes banner after each clic on arrows.
 function goCarousel () {
 const arrowLeft = document.querySelector(".arrow_left")
 const arrowRight = document.querySelector(".arrow_right")
-let max = slides.length -1 //because we start with the first banner
+let max = slides.length -1 // Because we start with the first banner.
 let index = 0
 
+// Calls all the dots 
 dotsIntegration()
 
 arrowRight.addEventListener("click", () => {
+  // Condition to go back to the first slide after the last one.
   if (index === max) {
     index = 0
     displayImgTxt(index, slides)
@@ -53,10 +54,12 @@ arrowRight.addEventListener("click", () => {
     index++
     displayImgTxt(index, slides)
   }
+  // Update the position of the full dot.
   dotSelected(index)
 })
 
 arrowLeft.addEventListener("click", () => {
+  // Condition to go to the last banner from the first one.
   if (index === 0) {
     index = max
     displayImgTxt(index, slides)
@@ -64,6 +67,7 @@ arrowLeft.addEventListener("click", () => {
     index--
     displayImgTxt(index, slides)
   }
+  // Updates the position of the full dot.
   dotSelected(index)
 })
 }
